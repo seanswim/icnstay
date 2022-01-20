be6bf191bfdae0d4756fd16d0b913682a7d28201
tiahwang44 <tiahwang44@gmail.com>
2022-01-20 23:46:43 +0900

    FIXED: typo

--- server/app.js
+++ server/app.js
@@ -8,34 +8,38 @@ dotenv.config();
 const app = express();
 const router = require('./router');
 
-const clientUrl = process.env.CLIENT_URL || 'https://loacalhost:3000';
+const clientUrl = process.env.CLIENT_URL || 'https://localhost:3000';
 
-app.use( express.json() );
-app.use( express.urlencoded({ extended: false }) );
-app.use( cors({
-  origin: clientUrl, 
-  credentials: true,
-  methods: '*'
-}) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: clientUrl,
+    credentials: true,
+    methods: '*',
+  })
+);
 app.use(cookieParser());
 
-app.use( '/', router );
+app.use('/', router);
 
 const serverPort = process.env.SERVER_PORT || 4000;
 
 let server;
 
-const privateKey = fs.readFileSync( __dirname + '/../key.pem', 'utf-8' );
-const certificate = fs.readFileSync( __dirname + '/../cert.pem', 'utf-8' );
+const privateKey = fs.readFileSync(__dirname + '/../key.pem', 'utf-8');
+const certificate = fs.readFileSync(__dirname + '/../cert.pem', 'utf-8');
 
-
-server = https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app);
+server = https.createServer(
+  {
+    key: privateKey,
+    cert: certificate,
+  },
+  app
+);
 
 server.listen(serverPort, () => {
-  console.log(`Server running at ${serverPort} port`)
+  console.log(`Server running at ${serverPort} port`);
 });
 
 module.exports = server;
